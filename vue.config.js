d225011db906b7b24e44ecfdbf56cb7f425b9c1e
Max Hellwig <mail@maxhellwig.de>
2019-01-25 09:52:51 +0100

    Update public path for gitlab

---
+++ vue.config.js
@@ -0,0 +1,6 @@
+// vue.config.js file to be place in the root of your repository
+// make sure you update `yourProjectName` with the name of your GitLab project
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/ichag/" : "/"
+};
